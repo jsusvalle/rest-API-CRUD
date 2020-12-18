17a1538da6e95130a85dda18494b831993b1085b
jsusvalle <jesusvalle2001@hotmail.com>
2020-12-18 15:22:32 -0500

    Imagenes route added

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -37,8 +37,28 @@ let verificaAdmin_role = (req, res, next) => {
     }
 };
 
+//* Verificar token para Imagenes
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if(err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
 
 module.exports = {
     verificaToken,
-    verificaAdmin_role
+    verificaAdmin_role,
+    verificaTokenImg
 }
